4f1cd6d6c0bb4cf9306ebea0e25a65703a4fe4b5
josanchezm <blacksaylvl@gmail.com>
2022-04-27 19:55:22 -0500

    cambios en el html y el script

--- script.js
+++ script.js
@@ -1,34 +1,28 @@
-// alert("recuerde ingresar los elementos del conjunto separados por coma");
+alert("recuerde ingresar los elementos del conjunto separados por coma");
 
 // codigo media aritmetica
 
 function calcularPromedio(lista) {
-    const sumaLista = lista.reduce(
+    let listaFloat = [];
+
+    for(i=0;i<lista.length;i++) {
+        listaFloat.push(parseFloat(lista[i]));
+    }
+
+    const sumaLista = listaFloat.reduce(
         function(valorAcumulado = 0,nuevoElemento) {
             const sumatoria = valorAcumulado + nuevoElemento;
-
-            console.log(`
-            valor acumulado: ${valorAcumulado}
-            nuevo elemento: ${nuevoElemento}
-            `);
-            console.log(`sumatoria: ${sumatoria}`);
-
             return sumatoria;
         }
     );
 
     const promedioLista = sumaLista/lista.length;
 
-    const resultado = {
-        sumatoria: sumaLista,
-        cantidad_elementos: lista.length, 
-        promedio: promedioLista
-    }
-    console.log(resultado);
+    const resultado = promedioLista.toFixed(2);
     return resultado;
 }
 
-calcularPromedio([700,450,300,275,100,95]);
+calcularPromedio([700,450,300,275,100.9,95]);
 
 function promedio() {
     const promedioInput = document.getElementById("conjunto-promedio");
@@ -39,21 +33,16 @@ function promedio() {
 
     function arrayInt() {
         for(i=0;i<arrayPromedioValue.length;i++) {
-            arrayPromedioValueInt.push(parseInt(arrayPromedioValue[i]));
+            arrayPromedioValueInt.push(parseFloat(arrayPromedioValue[i]));
         }
     }
     arrayInt();
     console.log(arrayPromedioValueInt);
 
     const calculoPromedio = calcularPromedio(arrayPromedioValueInt); 
-    const calculoPromedioArray = Object.values(calculoPromedio);
-    console.log(calculoPromedioArray);
 
     const resultado = document.getElementById("resultado-promedio"); 
-    resultado.innerText = `sumatoria: ${calculoPromedioArray[0]}
-    cantidad elementos: ${calculoPromedioArray[1]}
-    promedio: ${calculoPromedioArray[2]}
-    `;
+    resultado.innerText = `Promedio: ${calculoPromedio}`;
 }
 
 // codigo mediana
@@ -66,8 +55,6 @@ function esPar(numero) {
     }
 }
 
-
-
 function calcularMediana(lista) {
     lista.sort(
         function(a,b) {
@@ -75,11 +62,7 @@ function calcularMediana(lista) {
         }
     );
 
-    console.log(`array ordenado: [${lista}]`);
-
     const mitadLista = lista.length / 2;
-
-    console.log(esPar(lista.length));
 
     let mediana;
     let resultado;
@@ -89,18 +72,12 @@ function calcularMediana(lista) {
         const elemento2 = lista[mitadLista];
     
         mediana = calcularPromedio([elemento1,elemento2]);
+        resultado = mediana;
     
-        const arrayMediana = Object.values(mediana);
-
-        resultado = arrayMediana[2];
-    
-        console.log(`la mediana del array [${lista}] es: ${resultado[2]}`);
     } else {
         mediana = lista[parseInt(mitadLista)];
 
-        resultado = mediana;
-    
-        console.log(`la mediana del array [${lista}] es: ${mediana}`);
+        resultado = mediana.toFixed(2);
     }
     return resultado;
 }
@@ -111,22 +88,16 @@ function mediana() {
     const medianaInput = document.getElementById("conjunto-mediana");
     const medianaValue = medianaInput.value;
     const arrayMedianaValue = medianaValue.split(",");
-    console.log(arrayMedianaValue);
-    let arrayMedianaValueInt = [];
+    let arrayMedianaValueFloat = [];
 
-    function arrayInt() {
-        for(i=0;i<arrayMedianaValue.length;i++) {
-            arrayMedianaValueInt.push(parseInt(arrayMedianaValue[i]));
-        }
+    for(i=0;i<arrayMedianaValue.length;i++) {
+        arrayMedianaValueFloat.push(parseFloat(arrayMedianaValue[i]));
     }
-    arrayInt();
-    console.log(arrayMedianaValueInt);
 
-    const calculoMediana = calcularMediana(arrayMedianaValueInt); 
+    const calculoMediana = calcularMediana(arrayMedianaValueFloat); 
     const resultado = document.getElementById("resultado-mediana"); 
-    resultado.innerText = calculoMediana;
+    resultado.innerText = `Mediana: ${calculoMediana}`;
 }
-
 
 // codigo moda
 
@@ -162,8 +133,8 @@ function calcularModa(lista) {
                 moda.push(conjunto);
             }
             newModa();
-            resultado = `moda(s): ${moda}
-            repiticiones: ${listaArray[listaArray.length-1][1]}`;
+            resultado = `Moda(s): ${moda}
+            Repiticiones: ${listaArray[listaArray.length-1][1]}`;
         }   
     }
     console.log(resultado);
@@ -177,8 +148,66 @@ function moda() {
     const modaValue = modaInput.value;
     const arrayModaValue = modaValue.split(",");
     console.log(arrayModaValue);
-    
+
     const calculoModa = calcularModa(arrayModaValue); 
     const resultado = document.getElementById("resultado-moda"); 
     resultado.innerText = calculoModa;
 }
+
+// codigo promedio ponderado 
+
+function argumentosValidos(lista1,lista2) {
+    if(lista1.length === lista2.length) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function calcularPromedioPonderado(listaNumero,listaPonderado) {
+  
+    if(argumentosValidos(listaNumero,listaPonderado)) {
+        let sumatoriaPonderada = 0;
+        let divisor = 0;
+
+        for(i=0;i<listaNumero.length;i++) {
+            sumatoriaPonderada += listaNumero[i]*listaPonderado[i];
+            divisor += listaPonderado[i];
+        }
+
+        const promedioPonderado = sumatoriaPonderada/divisor;
+        const resultado = promedioPonderado.toFixed(2);
+        return resultado;
+    } else {
+        alert("Cada elemento debe contar con su ponderacion correspondiente y viceversa");
+    }
+}
+
+calcularPromedioPonderado([3.7,4,2.5,3],[1,3,3,2]);
+
+function Promedio_ponderado() {
+    const ponderadoInput = document.getElementById("promedio-ponderado-calculo");
+    const ponderadoValue = ponderadoInput.value; 
+    const ponderacionInput = document.getElementById("ponderacion-ponderado");
+    const ponderacionValue = ponderacionInput.value; 
+
+    let ponderadoValueArray = ponderadoValue.split(",");
+    let ponderacionValueArray = ponderacionValue.split(",");
+
+    if(argumentosValidos(ponderadoValueArray,ponderacionValueArray)) {
+        let ponderadoValueArrayFloat = [];
+        let ponderacionValueArrayFloat = [];
+
+        for(i=0;i<ponderadoValueArray.length;i++) {
+        ponderadoValueArrayFloat.push(parseFloat(ponderadoValueArray[i]));
+        ponderacionValueArrayFloat.push(parseFloat(ponderacionValueArray[i])); 
+        }
+
+        const calculoPonderado = calcularPromedioPonderado(ponderadoValueArrayFloat,ponderacionValueArrayFloat);
+        const resultado = document.getElementById("resultado-promedio-ponderado");
+        resultado.innerText = `Promedio ponderado: ${calculoPonderado}`;
+    } else {
+        alert("Cada elemento debe contar con su ponderacion correspondiente y viceversa");
+    }
+}
+
